9a51c03c90a29d65e60361e8a4f88111fb80bc66
sergiolfaria <srgion@yahoo.com.br>
2024-05-11 04:07:22 -0300

    pequenas alterações  de estilo

--- src/components/sidebar/ButtonLink.vue
+++ src/components/sidebar/ButtonLink.vue
@@ -20,9 +20,9 @@ export default {
 
 <template>
   
-  <div v-for="link in links" :key="link.title_route" class="w-72">
+  <div v-for="link in links" :key="link.title_route" class=" w-11/12">
     <router-link :to="link.link" class="flex gap-4 text-white py-2 px-6 text-2xl rounded-l-full bg-light-blue">
-      <i :class="link.icone"></i>
+      <i :class="link.icone " ></i>
       {{ link.title_route }}
     </router-link>
   </div>
